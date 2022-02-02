7ed4d215e5907662e784fc7e258d27d7ae64079a
Angel Fernandez Roji <angelitoh.fernandez@gmail.com>
2022-02-02 20:45:35 +0100

    Primer Modelo creado (02/02/2022)

--- __manifest__.py
+++ __manifest__.py
@@ -1,22 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "proyectos",
+    'name': "Modulos Odoo",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Media Shop Spain es una tienda de tecnología: Ordenadores, móviles, consolas.... ¡Tenemos de todo!
+        Por lo que necesitamos tener nuestros módulos actualizados.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Angel MMS",
+    'website': "http://infsalinas.sytes.net:10070/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Tecnología',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
