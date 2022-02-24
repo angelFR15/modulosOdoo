8c437f6796137144b73b2559255c9836097c8d38
Ángel Fernández <73905870+angelFR15@users.noreply.github.com>
2022-02-24 21:09:29 +0100

    24/02/2022 -- Metodo Valid hours 1

--- horarios/models/models.py
+++ horarios/models/models.py
@@ -26,16 +26,34 @@ class horarios(models.Model):
 
     #atributos
     nombreHorario = fields.Char(string='Nombre horario', required = True)
-    lunesEntrada = fields.Date (string='Hora entrada Lunes')
-    lunesSalida = fields.Date(string='Hora salida Lunes')
-    martesEntrada = fields.Date (string='Hora entrada Martes')
-    martesSalida = fields.Date(string='Hora salida Martes')
-    miercolesEntrada = fields.Date (string='Hora entrada Miercoles')
-    miercolesSalida = fields.Date(string='Hora salida Miercoles')
-    juevesEntrada = fields.Date (string='Hora entrada Jueves')
-    juevesSalida = fields.Date(string='Hora salida Jueves')
-    viernesEntrada = fields.Date (string='Hora entrada Lunes')
-    viernesSalida = fields.Date(string='Hora salida Lunes')
+
+    lunesEntrada = fields.Date (string='Hora entrada lunes', selection='_get_valid_hours', default='8.5')
+    lunesSalida = fields.Date(string='Hora salida lunes', selection='_get_valid_hours', default='8.5')
+    martesEntrada = fields.Date (string='Hora entrada martes', selection='_get_valid_hours', default='8.5')
+    martesSalida = fields.Date(string='Hora salida martes', selection='_get_valid_hours', default='8.5')
+    miercolesEntrada = fields.Date (string='Hora entrada miercoles', selection='_get_valid_hours', default='8.5')
+    miercolesSalida = fields.Date(string='Hora salida miercoles', selection='_get_valid_hours', default='8.5')
+    juevesEntrada = fields.Date (string='Hora entrada jueves', selection='_get_valid_hours', default='8.5')
+    juevesSalida = fields.Date(string='Hora salida jueves', selection='_get_valid_hours', default='8.5')
+    viernesEntrada = fields.Date (string='Hora entrada viernes', selection='_get_valid_hours', default='8.5')
+    viernesSalida = fields.Date(string='Hora salida viernes', selection='_get_valid_hours', default='8.5')
 
     #relacion con tabla empleados
     empleado_id = fields.One2many('proyectos.empleado','horario_id')
+
+    @api.model
+    def _get_valid_hours(self):
+        selection = [
+            ('8', '8:00'),   ('8.5', '8:30'),
+            ('9', '9:00'),   ('9.5', '9:30'),
+            ('10', '10:00'), ('10.5', '10:30'),
+            ('11', '11:00'), ('11.5', '11:30'),
+            ('12', '12:00'), ('12.5', '12:30'),
+            ('13', '13:00'), ('13.5', '13:30'),
+            ('14', '14:00'), ('14.5', '14:30'),
+            ('15', '15:00'), ('15.5', '15:30'),
+            ('16', '16:00'), ('16.5', '16:30'),
+            ('17', '17:00'), ('17.5', '17:30'),
+            ('18', '18:00')
+        ]
+        return selection
