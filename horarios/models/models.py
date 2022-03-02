14842d14be5b2e02b6e88d22617dfe9ae896bfac
Ángel Fernández <73905870+angelFR15@users.noreply.github.com>
2022-03-02 19:36:36 +0100

    02/03/2022 -- Correccion horas 3 clase

--- horarios/models/models.py
+++ horarios/models/models.py
@@ -27,19 +27,25 @@ class horarios(models.Model):
     #atributos
     nombreHorario = fields.Char(string='Nombre horario', required = True)
 
-    lunesEntrada = fields.Selection(string='Hora salida lunes', selection='_get_valid_hours', default='8')
+    lunesEntrada = fields.Selection(string='Hora entrada lunes', selection='_get_valid_hours', default='8')
     lunesSalida = fields.Selection(string='Hora salida lunes', selection='_get_valid_hours', default='8')
-    martesEntrada = fields.Selection(string='Hora salida martes', selection='_get_valid_hours', default='8')
+    martesEntrada = fields.Selection(string='Hora entrada martes', selection='_get_valid_hours', default='8')
     martesSalida = fields.Selection(string='Hora salida martes', selection='_get_valid_hours', default='8')
-    miercolesEntrada = fields.Selection(string='Hora salida miercoles', selection='_get_valid_hours', default='8')
+    miercolesEntrada = fields.Selection(string='Hora entrada miercoles', selection='_get_valid_hours', default='8')
     miercolesSalida = fields.Selection(string='Hora salida miercoles', selection='_get_valid_hours', default='8')
-    juevesEntrada = fields.Selection(string='Hora salida jueves', selection='_get_valid_hours', default='8')
+    juevesEntrada = fields.Selection(string='Hora entrada jueves', selection='_get_valid_hours', default='8')
     juevesSalida = fields.Selection(string='Hora salida jueves', selection='_get_valid_hours', default='8')
-    viernesEntrada = fields.Selection(string='Hora salida viernes', selection='_get_valid_hours', default='8')
+    viernesEntrada = fields.Selection(string='Hora entrada viernes', selection='_get_valid_hours', default='8')
     viernesSalida = fields.Selection(string='Hora salida viernes', selection='_get_valid_hours', default='8')
 
     #relacion con tabla empleados
     empleado_id = fields.One2many('proyectos.empleado','horario_id')
+
+    def name_get(self):
+        resultados=[]
+        for horario in self:
+            resultados.append((horario.id, horario.nombreHorario))
+        return resultados
 
     @api.model
     def _get_valid_hours(self):
@@ -58,9 +64,34 @@ class horarios(models.Model):
         ]
         return selection
 
-    @api.constrains('lunesEntrada','lunesSalida')
+    @api.constrains('lunesEntrada','martesEntrada','miercolesEntrada','juevesEntrada','viernesEntrada')
     def change_data_field(self):
         hel = float(self.lunesEntrada)
         hsl = float(self.lunesSalida)
         if(hel >= hsl):
-            raise exceptions.ValidationError("No puede ser anterior o igual la hora de salida a la de entrada.")
+            raise exceptions.ValidationError("Las horas del Lunes estan mal, revisalas (la hora de salida no puede ser anterior o igual a la de entrada).")
+        
+        hem = float(self.martesEntrada)
+        hsm = float(self.martesSalida)
+        if(hem >= hsm):
+            raise exceptions.ValidationError("Las horas del Martes estan mal, revisalas (la hora de salida no puede ser anterior o igual a la de entrada).")
+        
+        hex = float(self.miercolesEntrada)
+        hsx = float(self.miercolesSalida)
+        if(hex >= hsx):
+            raise exceptions.ValidationError("Las horas del Miercoles estan mal, revisalas (la hora de salida no puede ser anterior o igual a la de entrada).")
+
+        hex = float(self.miercolesEntrada)
+        hsx = float(self.miercolesSalida)
+        if(hex >= hsx):
+            raise exceptions.ValidationError("Las horas del Miercoles estan mal, revisalas (la hora de salida no puede ser anterior o igual a la de entrada).")
+
+        hej = float(self.juevesEntrada)
+        hsj = float(self.juevesSalida)
+        if(hej >= hsj):
+            raise exceptions.ValidationError("Las horas del Jueves estan mal, revisalas (la hora de salida no puede ser anterior o igual a la de entrada).")
+
+        hev = float(self.viernesEntrada)
+        hsv = float(self.viernesSalida)
+        if(hev >= hsv):
+            raise exceptions.ValidationError("Las horas del Viernes estan mal, revisalas (la hora de salida no puede ser anterior o igual a la de entrada).")
